32db1023c9de0d87f93f1a2a6d4714b7ba6a9e9d
Rafa <rafa@elementai.com>
2021-04-08 19:54:08 +0100

    Start product create section

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import './App.css';
 import Products from "./management/Products";
-import Users from "./users/Users";
 import {BrowserRouter, Route} from "react-router-dom";
+import Users from "./users/Users";
 
 function App() {
   return (
     <div className="App">
-
-            <BrowserRouter>
-              <Route path='/' exact component={Users}/>
-              <Route path='/management/products' component={Products}/>
-            </BrowserRouter>
-
+       <BrowserRouter>
+         <Route path='/' exact component={Users}/>
+         <Route path='/management/products' exact component={Products}/>
+         <Route path='/management/products/create' exact component={ProductsCreate}/>
+       </BrowserRouter>
     </div>
   );
 }
